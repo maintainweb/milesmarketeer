35f1cce964fcbde013304f3679e9aec99b475548
Erik James Albaugh <erik@maintainweb.co>
2014-05-23 21:13:42 -0700

    Custom Functions
    Add Post Types file
    Add ACF Functions (for options pages)
    Add content-branding template file

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,5 @@
 /**
  * Custom functions
  */
+require_once locate_template('/lib/post-types.php'); 			// Custom Post Types
+require_once locate_template('/lib/acf.php');          	// Advanced Custom Fields Additions
