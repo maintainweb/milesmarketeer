2907db1f46df6157220edcc296890d8abe2f8d74
Erik James Albaugh <erik@maintainweb.co>
2014-05-23 23:16:13 -0700

    Single Page Layout
    Additions to begin single page layout options

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0c6b02d660216d64d7529273755db445');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '11756f8eb82dfdb69ccdc627e64be94f');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
