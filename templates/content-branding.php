d50c9d3913d6ad6b50954f38236f5f094bb5784d
Erik James Albaugh <erik@maintainweb.co>
2014-05-23 23:15:43 -0700

    Branding Template
    Updated Branding Image Funcitons

--- templates/content-branding.php
+++ templates/content-branding.php
@@ -1,13 +1,7 @@
 <?php 
-        if( get_field('use_white_version', 'options') )
-        {
-          $brandimage = get_field('brand_image_white', 'options');
-        }
-        else
-        {
-          $brandimage = get_field('brand_image', 'options');
-        }
-
+        $brandimage = get_field('brand_image', 'options');
+        $brandimagevertical = get_field('brand_image_vertical', 'options');
+        $brandimagehorizontal = get_field('brand_image_horizontal', 'options');
         $primarycolor = get_field('primary_color', 'options');
         $secondarycolor = get_field('secondary_color', 'options');
         $tertiarycolor = get_field('tertiary_color', 'options');
@@ -21,7 +15,7 @@
           $caption = $brandimage['caption'];
          
           // sizes
-          $size = 'medium';
+          $size = 'full';
           $med = $brandimage['sizes'][ $size ];
           $medwidth = $brandimage['sizes'][ $size . '-width' ];
           $medheight = $brandimage['sizes'][ $size . '-height' ];
@@ -29,11 +23,8 @@
 ?>
 
   <style>
-    .brand {
-      background-image: url(<?php echo $med; ?>);
-      background-color: <?php echo $secondarycolor; ?>;
-      width: <?php echo $medwidth; ?>;
-      height: <?php echo $medheight; ?>;
+    .navbar-brand {
+      background-image: url(<?php echo $url; ?>);
     }
   </style>
 
