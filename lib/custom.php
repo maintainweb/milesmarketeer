0e39d802d829bc479bf3c1d1edd6de837607133b
Erik James Albaugh <erik@maintainweb.co>
2014-05-24 22:39:01 -0700

    Custom Functions for ACF
    Count Columns function
    Post Object template

--- lib/custom.php
+++ lib/custom.php
@@ -4,3 +4,11 @@
  */
 require_once locate_template('/lib/post-types.php'); 			// Custom Post Types
 require_once locate_template('/lib/acf.php');          	// Advanced Custom Fields Additions
+
+
+function count_columns() {
+	$rows = get_field('column');
+	$row_count = count($rows);
+	 
+	echo $row_count;
+}
