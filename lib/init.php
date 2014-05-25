9a2878957db9af84e7b732984f2da37288cd4877
Erik James Albaugh <erik@maintainweb.co>
2014-05-24 22:38:29 -0700

    Text Domain Name Change
    leoken to milesmarketeer

--- lib/init.php
+++ lib/init.php
@@ -8,7 +8,7 @@ function roots_setup() {
 
   // Register wp_nav_menu() menus (http://codex.wordpress.org/Function_Reference/register_nav_menus)
   register_nav_menus(array(
-    'primary_navigation' => __('Primary Navigation', 'leoken'),
+    'primary_navigation' => __('Primary Navigation', 'milesmarketeer'),
   ));
 
   // Add post thumbnails (http://codex.wordpress.org/Post_Thumbnails)
--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '0c760aae05d6a2e9f0215f91c503f012');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '4ff7fe749047dfc4192f7482b63fd008');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
