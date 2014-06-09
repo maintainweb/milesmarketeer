24ae2fe3df71c81c3c61c2268fbcc18b28ff3b3e
Erik James Albaugh <erik@maintainweb.co>
2014-06-08 22:56:06 -0700

    Google Font Added

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '552cda780d4d7e8d2989adbf8518b0bc');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'de9c218413a5d6a9e1814bb027805341');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
