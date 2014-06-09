9701f9d36436aa22976d4b75b8beb2395bc3d5cb
Erik James Albaugh <erik@maintainweb.co>
2014-06-09 07:27:17 -0700

    New ACF Background fields
    Add Background Size field (acf)
    Add Background Repeat field (acf)

--- templates/content-section-background.php
+++ templates/content-section-background.php
@@ -2,6 +2,8 @@
 global $r;
 $background_image = get_sub_field('section_background_image');
 $background_color = get_sub_field('section_background_color');
+$background_size = get_sub_field('section_background_size');
+$background_repeat = get_sub_field('section_background_repeat');
 
   if( !empty($background_image) ):
 
@@ -26,5 +28,16 @@ endif; ?>
   <?php if( !empty($background_color) ): ?>
       background-color: <?php echo $background_color; ?>;
   <?php endif; ?>
+  <?php if( !empty($background_size) ): ?>
+      background-size: <?php echo $background_size; ?>;
+  <?php endif; ?>
+  <?php if( !empty($background_repeat) ): ?>
+      background-repeat: <?php echo $background_repeat; ?>;
+  <?php endif; ?>
   }
+  <?php if( empty($background_image) ): ?>
+  .single-page-section-<?php echo $r; ?>:before, .single-page-section-<?php echo $r; ?>:after {
+    display: none!important;
+  }
+  <?php endif; ?>
 </style>
