d092430c5259997abd9a6d9c20f44710f998f94a
Erik James Albaugh <erik@maintainweb.co>
2014-06-10 15:08:41 -0700

    Styles & Scripts
    Gravity Forms Style Updates
    Footer Style Updates

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'f408f5a2bc8bd2f38904be94d3653fca');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c78faf3305640f969c2118d1934116e6');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
