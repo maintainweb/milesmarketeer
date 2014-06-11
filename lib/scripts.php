21a890c49c4e85fad18a654ba6d4d0a42cab1828
Erik James Albaugh <erik@maintainweb.co>
2014-06-11 13:12:01 -0700

    Styles & Scripts
    Form Styles Updated

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'c78faf3305640f969c2118d1934116e6');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '624377f2458e179be62322235d3c0db1');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
